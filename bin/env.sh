f56c9e505c76160db963534a1e0d3111c8c23e72
Andy Cunningham <a.cunning1@ymail.com>
2017-03-02 19:26:24 -0800

    deploy target integration

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,7 @@
 export DOCKER_REPO=acunning1
 export DOCKER_IMAGE=homeless-service
 export PROJ_SETTINGS_DIR=homelessAPI
-export DEPLOY_TARGET=LOCAL
+export DEPLOY_TARGET=INTEGRATION
 echo "##############################"
 echo  Your Local Project Environment
 echo "##############################"
